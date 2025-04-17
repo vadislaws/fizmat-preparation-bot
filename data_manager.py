d7f6a9e1f1cf796e6f88ea9481d706277d46d187
vadislaws <vladislav.tkachenko26@fizmat.kz>
2025-04-17 23:11:59 +0500

    Clean version without secret

--- data_manager.py
+++ data_manager.py
@@ -25,9 +25,3 @@ def save_users(data):
 def load_tests():
     with open("data/tests.json", "r", encoding="utf-8") as f:
         return json.load(f)
-
-from openai import OpenAI
-
-client = OpenAI(
-    api_key="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" 
-)
